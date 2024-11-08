65257b211b65e95f4a103ed04117f482b6ebf6d7
Kingyoyo76 <186572582+Kingyoyo76@users.noreply.github.com>
2024-11-08 17:48:38 -0500

    Fix syntax error in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
-      },
-    ],
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-};
+}
 
 module.exports = nextConfig;
-eslint: {
-  ignoreDuringBuilds: true,
-}
